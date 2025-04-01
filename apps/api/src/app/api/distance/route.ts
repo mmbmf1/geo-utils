230e84209a82f40c297aaa3210dd77f9dc684069
Michael Mace <michael.mace@conexon.us>
2025-04-01 09:33:49 -0500

    add more logs

--- apps/api/src/app/api/distance/route.ts
+++ apps/api/src/app/api/distance/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   try {
     console.log('Database URL:', process.env.POSTGRES_URL)
+    console.log('test', process.env.DATABASE_URL)
     console.log('Request received:', request.url)
     const { point1, point2, unit = 'miles' } = await request.json()
 
